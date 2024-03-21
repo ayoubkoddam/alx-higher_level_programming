15f01c187ec4d7c2f5849239b98e5864592c25a0
ayoubkoddam <ayoubkoddam@gmail.com>
2024-03-20 17:31:39 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string: str):
-    if roman_string is None or type(roman_string) != str:
-        return 0
-    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numbers = [data[x] for x in roman_string] + [0]
-    rep = 0
+""" Roman to Integer test file
+"""
+roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-    for i in range(len(numbers) - 1):
-        if numbers[i] >= numbers[i+1]:
-            rep += numbers[i]
-        else:
-            rep -= numbers[i]
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-    return rep
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-Advance task
-vi 100-weight_average.py
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-
-    num = 0
-    den = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
