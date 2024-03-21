b376ea35b3d2d607687a1462383039509bdd450d
ayoubkoddam <ayoubkoddam@gmail.com>
2024-03-20 17:35:12 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
-        return 0
-    total = 0
-    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for roman in reversed(roman_string):
-        arabic = digits[roman]
-        total += arabic if total < arabic * 5 else -arabic
-    return total
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    p = 0
+
+    if type(roman_string) is str and roman_string:
+        for c in range(len(roman_string) - 1, -1, -1):
+            if val[roman_string[c]] >= p:
+                res += val[roman_string[c]]
+            else:
+                res -= val[roman_string[c]]
+            p = val[roman_string[c]]
+    return res
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    nlist = set(my_list)
-    return sum(nlist) 
+    number = 0
+    for element in set(my_list):
+        number += element
+        return  
